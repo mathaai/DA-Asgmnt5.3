0481faf14c4f09c068a9fe641c54b0c955ce1e08
Mathew K <kumpalamthanam@gmail.com>
2018-05-14 22:00:47 +0530

    Update Assignment 53.R

--- Assignment 53.R
+++ Assignment 53.R
@@ -13,7 +13,7 @@ sort(vec2, decreasing = T)
 str(vec1)
 paste(vec1,vec2, sep = ",")
 
-#str() gives the structure of an object where as paste() function concatenates one or more vectors to a character vector
+#str() gives the structure of an object and a compact preview of contents where as paste() function concatenates one or more vectors to a character vector or collapses the existing vector to a set of chars. str() can have only 1 object as input whereas paste() can have 1 or more objects as inputs
 
 #4. Introduce a separator when concatenating the strings.
 a <- "String1"
